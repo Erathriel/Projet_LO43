c582edac1ae7d2bbf676275325c1a7635ae1c7ee
Erathriel <geoffreynoro@orange.fr>
2018-12-21 15:45:53 +0100

    Fenetre de vue de la partie - Modifié

--- src/Vue/PartieVue.java
+++ src/Vue/PartieVue.java
@@ -2,18 +2,43 @@ package Vue;
 
 import Modele.*;
 
+import javax.swing.*;
 import java.io.*;
 
-public class PartieVue {
+public class PartieVue extends JFrame {
     private PartieModele mpartie;
+    private JLabel labelTest;
 
     // Constructeur
     public PartieVue(PartieModele partie) {
+        super();
         this.mpartie = partie;
+        this.setTitle("Map");
+        setExtendedState(this.getExtendedState() | this.MAXIMIZED_BOTH);
+        initAttribut();
+        creerFenetre();
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     // Methodes
-    public void affichageMapConsole(String name){
+
+    // Init des attribut de la fenetre
+    public void initAttribut(){
+        this.labelTest = new JLabel("Test");
+        labelTest.setBorder(null);
+        labelTest.setOpaque(false);
+    }
+
+    // Creation de la fenetre avec l'ajout des different componsant
+    public void creerFenetre(){
+        JPanel panTest = new JPanel();
+        panTest.add(labelTest);
+        panTest.setOpaque(false);
+        this.setContentPane(panTest);
+    }
+
+    /*public void affichageMapConsole(String name){
         FileInputStream fis = null;
         BufferedInputStream bis = null;
         //String map = "maps\\mapTest.txt";
@@ -37,6 +62,34 @@ public class PartieVue {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }*/
+
+    // Methode d'affichage renvoie String
+    public String affichageMapConsole(String name){
+        String ligne="";
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        //String map = "maps\\mapTest.txt";
+        //System.out.println(map);
+        File file = new File(name);
+        System.out.println(file.getAbsoluteFile());
+        try {
+            fis = new FileInputStream(file.getAbsoluteFile());
+            bis = new BufferedInputStream(new FileInputStream(file.getAbsoluteFile()));
+            //byte[] buffer = new byte[8];
+            int buffer;
+            while((buffer = fis.read()) != -1){
+                //System.out.println((char) buffer);
+                ligne= ligne + (char) buffer;
+            }
+            System.out.println(ligne);
+            bis.close();
+            fis.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ligne;
     }
 
     // Getters et Setters
@@ -48,5 +101,11 @@ public class PartieVue {
         this.mpartie = partie;
     }
 
+    public JLabel getLabelTest() {
+        return labelTest;
+    }
 
+    public void setLabelTest(JLabel labelTest) {
+        this.labelTest = labelTest;
+    }
 }
