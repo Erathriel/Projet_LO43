88b1bc0d5406f6516b5210d4546783f54fcd6c11
Geoffrey <geoffreynoro@orange.fr>
2019-01-09 21:05:06 +0100

    Interface graphique OK

--- src/Vue/GameFen.java
+++ src/Vue/GameFen.java
@@ -8,30 +8,17 @@ package Vue;
 public class GameFen extends JFrame {
     private PartieModele mpartie;
     private GamePane gamePane;
-    private PlateauPanel plateau;
+
     GameFen(PartieModele partie){
         super();
         this.mpartie = partie;
         this.setTitle("Jeu");
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        initAttribut();
-        creerFenetre();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.gamePane=new GamePane(mpartie);
         this.add(gamePane);
         this.setVisible(true);
 
-    }
-    // Init des attributs de la fenetre
-    public void initAttribut(){
-        this.plateau = new PlateauPanel(mpartie.getPlateau());
-        plateau.setBorder(null);
-        plateau.setOpaque(false);
-    }
-
-    // Creation de la fenetre avec l'ajout des different componsant
-    public void creerFenetre(){
-        this.setContentPane(plateau);
     }
 
 
@@ -44,13 +31,6 @@ public class GameFen extends JFrame {
         this.mpartie = partie;
     }
 
-    public PlateauPanel getPlateau() {
-        return plateau;
-    }
-
-    public void setPlateau(PlateauPanel plateau) {
-        this.plateau = plateau;
-    }
     public void repaintInfo(ProfModele p){
         this.gamePane.getGamePanelInfo().setJoueur(p);
         this.gamePane.getGamePanelInfo().repaint();
--- src/Vue/GamePane.java
+++ src/Vue/GamePane.java
@@ -8,15 +8,30 @@ import java.awt.*;
 
 public class GamePane extends JPanel {
     private GamePanelInfo gamePanelInfo;
+    private PlateauPanel plateau;
+    private PartieModele mpartie;
     GamePane(PartieModele mPartie){
+        this.mpartie = mPartie;
         this.setLayout(new BorderLayout());
         this.gamePanelInfo=new GamePanelInfo(mPartie.getJoueurEnAction());
         this.add(new GameButtonpane(),BorderLayout.PAGE_END);
-        PlateauPanel map=new PlateauPanel(new PlateauModele(16));
-        map.setPreferredSize(new Dimension(600,600));
-        this.add(map,BorderLayout.CENTER);
+        initAttribut();
+        this.add(plateau,BorderLayout.CENTER);
         this.add(this.gamePanelInfo,BorderLayout.LINE_START);
         this.setVisible(true);
+    }
+    // Init des attributs de la fenetre
+    public void initAttribut(){
+        this.plateau = new PlateauPanel(mpartie.getPlateau());
+        plateau.setBorder(null);
+        plateau.setOpaque(false);
+    }
+    public PlateauPanel getPlateau() {
+        return plateau;
+    }
+
+    public void setPlateau(PlateauPanel plateau) {
+        this.plateau = plateau;
     }
     public GamePanelInfo getGamePanelInfo(){
         return this.gamePanelInfo;
