8439d2d3a559be1a3957d657ba5d001dd3254a89
Geoffrey <geoffreynoro@orange.fr>
2019-01-10 21:45:42 +0100

    Menu OK - pas tout a fait

--- src/Application.java
+++ src/Application.java
@@ -11,5 +11,6 @@ import java.util.Arrays;
 public class Application {
     public static void main(String[] args) {
         MenuFen fen = new MenuFen();
+        MenuControlleur mControl = new MenuControlleur(fen);
     }
 }
