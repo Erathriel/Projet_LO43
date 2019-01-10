a82dded800931d0340c348eb4a29e167008cf8f8
Geoffrey <geoffreynoro@orange.fr>
2019-01-10 22:17:40 +0100

    Menu OK - pas tout a fait

--- src/Application.java
+++ src/Application.java
@@ -10,7 +10,12 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        MenuFen fen = new MenuFen();
-        MenuControlleur mControl = new MenuControlleur(fen);
+        //MenuFen fen = new MenuFen();
+        //MenuControlleur mControl = new MenuControlleur(fen);
+        PartieModele mPartie=new PartieModele();
+        GameFen g= new GameFen(mPartie);
+        PartieControlleur control=new PartieControlleur(mPartie,g);
+        mPartie.creationPersoObjM();
+        control.jeu();
     }
 }
